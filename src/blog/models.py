40198d36948e15c22491c5ca7e72e90878f6e918
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-16 17:36:24 -0700

    get post newslink contact model set up

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from organizer.models import Startup, Tag
+
+
+class Post(models.Modes):
+
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(max_length=63)
+    text = models.TextField()
+    pub_date = models.DateField()
+    pub_date = models.DateField()
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startup)
