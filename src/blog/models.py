82c32b812fd20dc7145409e760ee748a46e1fbbf
Ahrav Dutta <ahravdutta02@gmail.com>
2019-06-16 17:55:43 -0700

    refacor models to include str method

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,14 +1,29 @@
-from django.db import models
-
+from django.db.models import (
+    CharField,
+    SlugField,
+    TextField,
+    DateField,
+    ManyToManyField,
+    Model,
+)
 from organizer.models import Startup, Tag
 
 
-class Post(models.Modes):
+class Post(Model):
 
-    title = models.CharField(max_length=63)
-    slug = models.SlugField(max_length=63)
-    text = models.TextField()
-    pub_date = models.DateField()
-    pub_date = models.DateField()
-    tags = models.ManyToManyField(Tag)
-    startups = models.ManyToManyField(Startup)
+    title = CharField(max_length=63)
+    slug = SlugField(max_length=63)
+    text = TextField()
+    pub_date = DateField()
+    pub_date = DateField()
+    tags = ManyToManyField(Tag)
+    startups = ManyToManyField(Startup)
+
+    class Meta:
+        get_latest_by = "pub_date"
+        ordering = ["-pub_date", "title"]
+        verbose_name = "blog post"
+
+    def __str__(self):
+        date_string = self.pub_date.strftime("%Y-%m-%d")
+        return f"{self.title} on {date_string}"
--- src/organizer/models.py
+++ src/organizer/models.py
@@ -1,37 +1,66 @@
-from django.db import models
+from django.db.models import (
+    CharField,
+    DateField,
+    ManyToManyField,
+    Model,
+    SlugField,
+    TextField,
+    URLField,
+    EmailField,
+)
 
 
-class Tag(models.Model):
+class Tag(Model):
 
-    name = models.CharField(max_length=31, unique=True)
-    slug = models.SlugField(
+    name = CharField(max_length=31, unique=True)
+    slug = SlugField(
         max_length=31,
         unique=True,
         help_text="A label for url config",
     )
 
+    class Meta:
+        ordering = ["name"]
 
-class Startup(models.Model):
+    def __str__(self):
+        return self.name
 
-    name = models.CharField(max_length=31, db_index=True)
-    slug = models.SlugField(
+
+class Startup(Model):
+
+    name = CharField(max_length=31, db_index=True)
+    slug = SlugField(
         max_length=31,
         unique=True,
         help_text="A label for url config",
     )
-    description = models.TextField()
-    founded_date = models.DateField("date founded")
-    contact = models.EmailField()
-    website = models.URLField(max_length=255)
-    tags = models.ManyToManyField(Tag)
+    description = TextField()
+    founded_date = DateField("date founded")
+    contact = EmailField()
+    website = URLField(max_length=255)
+    tags = ManyToManyField(Tag)
+
+    class Meta:
+        get_latest_by = "founded_date"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
 
 
-class NewsLink(models.Model):
+class NewsLink(Model):
 
-    title = models.CharField(max_length=31)
-    slug = models.SlugField(max_length=31)
-    pub_date = models.DateField("date published")
-    link = models.URLField(max_length=255)
-    startup = models.ForeignKey(
-        Startup, on_delete=models.CASCADE
-    )
+    title = CharField(max_length=31)
+    slug = SlugField(max_length=31)
+    pub_date = DateField("date published")
+    link = URLField(max_length=255)
+    startup = ForeignKey(Startup, on_delete=CASCADE)
+
+    class Meta:
+        get_latest_by = "pub_date"
+        ordering = ["-pub_date"]
+        unique_together = ("slug", "startup")
+        verbose_name = "news article"
+
+    def __str__(self):
+        return f"{self.startup}: {self.title}"
